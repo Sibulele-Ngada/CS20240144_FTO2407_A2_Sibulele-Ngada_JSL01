88f42deed410c9cc65c838ac8875cacddc039e15
Sibulele Ngada <sibulelengada@gmail.com>
2024-08-29 23:04:29 +0200

    Add regular expression variable and logic to test input

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
+    let regEx = /pet_[A-Z a-z 0-9]+$/;
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
+        if (regEx.test(input)){
+            result = "Good";
+        } else {
+            result = "Not good";
+        }
             document.getElementById('result').innerText = result;
 }
 
